85a132b0396550e299563d2f2ebd378f2c3917e2
Quach Tran Anh <quachtrananh1609@gmail.com>
2026-05-20 13:58:40 +0700

    Add files via upload

--- bt4.py
+++ bt4.py
@@ -1,7 +1,7 @@
 count = 0
 lucky_number = 369
 
-while count < 6 :
+while count < 5:
     print(f"Bạn đang còn {5-count} lượt nhập")
     check_lucky_number = int(input("Nhập số may mắn có chứa 3 chữ số: "))
     if check_lucky_number < lucky_number:
@@ -10,9 +10,10 @@ while count < 6 :
     elif check_lucky_number > lucky_number:
         print("Số bạn vừa nhập đang lớn hơn số may mắn!")
         count+=1
-    if count > 5 :
+    elif count >= 5 :
         print("Bạn đã hết lượt và chúc may mắn lần sau!")
         print("--- Trò chơi kết thúc ---")
+
     else:
         print("Bạn đã đoán chính xác số may mắn!")
         count = 6
